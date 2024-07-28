8e62d4c3c0d5a728433c53d136a4adc9f4fce71d
Shivendra Saurav <shivendrasaurav@gmail.com>
2024-07-28 15:10:06 +0530

    added cors allow all in newservice template

--- fapi_server/services/newservice/main.py
+++ fapi_server/services/newservice/main.py
@@ -1,9 +1,23 @@
 # Use this file if NewService has to be run as standalone service
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from services.newservice.endpoints import router as newservice_router
 
 app = FastAPI()
+
+# CORS configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+  
 
 # Include the service router
 app.include_router(newservice_router, prefix="/newservice")
